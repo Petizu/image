76d0c61e85899e298d5c9901716178dfd842a9ff
Petizu <petterson.ryan@estudante.ifmt.edu.br>
2026-03-27 09:28:58 -0400

    Add files via upload

--- controller/ImagesController.java
+++ controller/ImagesController.java
@@ -1,25 +1,8 @@
 package io.spring.image.demo.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-
-@RestController
-@Slf4j
-@RequestMapping("/upload")
-@NotNull
-
-
 public class ImagesController {
     //*
-    // {"name": "", "size":100} //application/json.+
+    // {"name": "", "size":100} //application/json
     //*
 
     // mult-part/formdata
